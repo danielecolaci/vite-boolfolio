0f463bed8e7963c82c8ed9c767c074a374fcf086
Daniele <danielecolaci@outlook.com>
2024-05-30 18:13:41 +0200

    Installed Axios and set api call to see projects data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
+import axios from 'axios';
+
 
 export default {
   name: 'App',
   data() {
     return {
+      base_api_url: 'http://127.0.0.1:8000',
+      base_projects_url: '/api/projects',
       projects: []
     }
+  },
+  methods: {
+    callApi(url) {
+
+      axios
+        .get(url)
+        .then(response => {
+          /* console.log(response); */
+          this.projects = response.data;
+        })
+        .catch(err => {
+          console.error(err);
+        })
+
+    }
+  },
+  mounted() {
+
+    let url = this.base_api_url + this.base_projects_url;
+    this.callApi(url);
   }
 }
 
