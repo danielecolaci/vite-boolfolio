2251aaf9d871c7aede12d6b4dc49153731f7983f
Daniele <danielecolaci@outlook.com>
2024-06-03 17:09:41 +0200

    Add Content Data to single project view

--- src/views/ProjectShow.vue
+++ src/views/ProjectShow.vue
@@ -33,13 +33,42 @@ export default {
 </script>
 
 <template>
-    <div class="container">
+    <div class="container mt-5">
         <div v-if="project">
-            <h1>{{ project.title }}</h1>
+            <div class="row">
+                <div class="col-md-8">
+                    <img v-if="project.image.startsWith('uploads')" :src="base_api_url + '/storage/' + project.image"
+                        alt="Project Image" class="img-fluid rounded">
+                    <img v-else :src="project.image" alt="Project Image" class="img-fluid rounded">
+                </div>
+                <div class="col-md-4">
+                    <h1>{{ project.title }}</h1>
+                    <h6 class="text-muted">{{ project.subtitle }}</h6>
+                    <div class="badge bg-info text-white mb-3">
+                        {{ project.type.name }}
+                    </div>
+                    <p>{{ project.description }}</p>
+                    <a :href="project.url_code" class="btn btn-outline-info btn-sm me-2" target="_blank">Code</a>
+                    <a :href="project.url_web" class="btn btn-outline-info btn-sm" target="_blank">Web</a>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col">
+                    <h3>Technologies Used</h3>
+                    <ul class="list-inline">
+                        <li class="list-inline-item" v-for="tech in project.technologies" :key="tech.id">
+                            <span class="badge bg-secondary">{{ tech.name }}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
         <div v-else>
             <p>Loading...</p>
         </div>
+        <router-link to="/projects" class="btn btn-info text-white mb-3">
+            Torna ai progetti
+        </router-link>
     </div>
 </template>
 
